9479848783248fb8af4ae12b95649017577f5286
revoxhere <50244265+revoxhere@users.noreply.github.com>
2023-12-31 16:31:54 +0100

    Update logging.js

--- src/logging.js
+++ src/logging.js
@@ -1,7 +1,7 @@
 /* Duino-Coin Pool logging system
 For documention about these functions see
 https://github.com/revoxhere/duino-coin/blob/useful-tools
-2019-2022 Duino-Coin community */
+2019-2024 Duino-Coin community */
 
 const chalk = require("chalk");
 const blue = chalk.blue;
