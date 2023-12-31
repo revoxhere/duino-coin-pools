3edd57048230c8f709fd90d7bbd119cf864b91a9
revoxhere <50244265+revoxhere@users.noreply.github.com>
2023-12-31 16:31:23 +0100

    Update sync.js

--- src/sync.js
+++ src/sync.js
@@ -1,7 +1,7 @@
 /* Duino-Coin Pool Sync handler
 For documention about these functions see
 https://github.com/revoxhere/duino-coin/blob/useful-tools
-2019-2022 Duino-Coin community */
+2019-2024 Duino-Coin community */
 
 const net = require("net");
 const axios = require("axios");
